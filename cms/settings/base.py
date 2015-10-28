6075ea9ab20e8a39508d1e16c03f256ce244c2dc
Alexey <od-5@yandex.ru>
2015-10-28 14:36:44 +0300

    fix images

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -7,14 +7,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = 'u@5!he@8y6ynbta9c9(l=%b1qzb(c=*9*v)jf+1lkn%_by!jk*'
 
-if socket.gethostname() == 'alexy.pythonanywhere.com':
-    DEBUG = False
-else:
+if socket.gethostname() == 'alexy-PC':
     DEBUG = True
+else:
+    DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['enjoy-sa.ru', 'www.enjoy-sa.ru']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'admin@enjoy-africa.ru'
+EMAIL_HOST = 'smtp.fullspace.ru'
+EMAIL_HOST_USER = 'admin@enjoy-africa.ru'
+EMAIL_HOST_PASSWORD = 'alena2010'
 
 ROOT_URLCONF = 'cms.urls'
 
