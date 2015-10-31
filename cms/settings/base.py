34ee091b003db0f9905eba8b545978c87c34a3c3
Alexey <od-5@yandex.ru>
2015-10-31 20:49:02 +0300

    add model and templates

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -7,11 +7,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = 'u@5!he@8y6ynbta9c9(l=%b1qzb(c=*9*v)jf+1lkn%_by!jk*'
 
-# if socket.gethostname() == 'alexy-PC':
-#     DEBUG = True
-# else:
-#     DEBUG = False
-DEBUG = True
+if socket.gethostname() == 'alexy-PC':
+    DEBUG = True
+else:
+    DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 
