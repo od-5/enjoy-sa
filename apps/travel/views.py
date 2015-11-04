3865fb830ac74b03c50e0a893662daadafd6fa4f
Alexey <od-5@yandex.ru>
2015-11-05 00:49:02 +0300

    fix travel author-list class view

--- apps/travel/views.py
+++ apps/travel/views.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.shortcuts import get_object_or_404
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, ListView
+from core.models import User
 from .models import Travel
 from .forms import TravelCreateForm
 
@@ -16,3 +17,14 @@ class TravelCreateView(CreateView):
         return {
             'user': username,
         }
+
+
+class TravelListView(ListView):
+    # extra_context = {}
+
+    def get_context_data(self, **kwargs):
+        context = super(TravelListView, self).get_context_data(**kwargs)
+        context.update({
+            'author_list': User.objects.filter(first_name__isnull=False, last_name__isnull=False).exclude(avatar='')
+        })
+        return context
