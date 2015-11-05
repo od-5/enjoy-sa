7f336351028fb468dbf78ed33b01245dbfaf5b36
Alexey <od-5@yandex.ru>
2015-11-05 22:57:29 +0300

    fix core.models

--- core/models.py
+++ core/models.py
@@ -127,11 +127,8 @@ class Setup(models.Model):
         else:
             return u'Настройки'
 
-    meta_title = models.CharField(verbose_name=u'Заголовок <TITLE>...</TITLE>', max_length=256, blank=True)
     email = models.EmailField(verbose_name=u'e-mail для приёма заявок', blank=True)
     # video = models.TextField(verbose_name=u'HTML-код видео', blank=True, null=True)
-    meta_key = models.TextField(verbose_name=u'Ключевые слова META_KEYWORDS', blank=True)
-    meta_desc = models.TextField(verbose_name=u'Описание META_DESCRIPTION', blank=True)
     top_js = models.TextField(verbose_name=u'Скрипты в <HEAD>..</HEAD>', blank=True)
     bottom_js = models.TextField(verbose_name=u'Скрипты перед закрывающим </BODY>', blank=True)
     robots_txt = models.TextField(verbose_name=u'robots.txt', blank=True, null=True)
