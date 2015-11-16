7710d750970367e9f83e0a45410173c46e9e8c1a
Alexey <od-5@yandex.ru>
2015-11-16 17:27:58 +0300

    fix travel_list.html

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = 'u@5!he@8y6ynbta9c9(l=%b1qzb(c=*9*v)jf+1lkn%_by!jk*'
 
-if socket.gethostname() == 'r420':
+if socket.gethostname() == 'alexy-PC':
     DEBUG = True
 else:
     DEBUG = False
